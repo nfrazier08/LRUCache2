076e29d2892f3e0cb5cd0baedc975a0b8d4fb9ff
neliza <nicolasaelf64@gmail.com>
2019-06-08 23:35:03 -0600

    error handling for long/lat inputs checks out

--- index.js
+++ index.js
@@ -2,88 +2,78 @@
 
 // Dependencies
 // =============================================================
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const Cache = require("lru-cache-node")
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var Cache = require("lru-cache-node")
 
 // Sets up the Express App
 // =============================================================
-const app = express();
-const PORT = 3000;
+var app = express();
+var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// // Sets up the Express app to handle data parsing
+app.use(bodyParser.json({type: 'application/json'}));
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 
-//Set up cache
+
+// //Set up cache
 let cache = new Cache(3);
 
 cache.set('a', 7);
 cache.set('b', 5);
 cache.set('c', 3);
-// console.log(cache);
-// console.log("setting 10");
-// cache.set('d', 10);
-// console.log("cache");
-// console.log(cache);
+
 
 //Functions
-function GetImageURL(){
+function getImageURL(){
     console.log("yep I can get here!")
+
+    //Inside this function, I need to handle the following:
+    //If the user enters an accepted lon/lat value (Returm random number in string form :
+       //1) Cache hit- Return a cached URL
+       //2) Cache miss
+            //app gets a new URL and adds it to cache
+            //if cache is full, eject oldest item
+    
 }
 
-
 //PATHS
-
-
-//Home path
-app.get('/', function(req, res) {
+//Home Path - This is mainly for testing purposes at the moment
+app.get('/', function(req, res, next) {
     //toArray(): returns the cache in array form
-    var imagesCache = cache.toArray()
+  var imagesCache = cache.toArray()
     
-    //I can see all the items in the cache printed to my root local host
-    res.send(imagesCache)    
+  //I can see all the items in the cache printed to my root local host
+  res.send(imagesCache) 
 });
 
 
 //Long/Lat Path
-app.get("/api/mars/:latitude/:longitude", (req, res) => {
+app.get("/api/mars/:latitude/:longitude", (req, res, next) => {
    
     //These are the parameters and they need to be numbers
-    // var latitude = parseInt(req.params.latitude);
-    // var longitude = parseInt(req.params.longitude);
-    // error = {}
+    var latitude = parseInt(req.params.latitude);
+    var longitude = parseInt(req.params.longitude);
 
-    // //error handling
-    // //Latitude -90 to 90
-    // if(latitude > 90){
-    //     rs.send(error.latitude = 'lat is above accepted range')
-    // } else if (latitude < -90){
-    //     //lat is below accepted range
-    // } else if (isNaN(latitude)){
-    //     //Input is not an integer
-    // } else{
-    //     //entered lat is accepted
-    // }
+    //Latitude Range
+    //-90 to 90
 
-    // //Long -180 to 180
-    // if(longitude > 180){
-    //     error.longitude = 'lat is above accepted range'
-    // } else if (longitude < -180){
-    //     //long is below accepted ranges
-    // } else if (isNaN(longitude)){
-    //     //Long input is not an integer
-    // } else{
-    //     //entered long was accepted
-    // }
+    //Longitude Range
+    //-180 to 180
+    
+    if(latitude > 90 || longitude > 180){
+        res.send("latitude or longitude is ABOVE accepted range: \n latitude max: 90 \n longitude max: 180")
+    } else if (latitude < -90 || longitude < -180){
+        res.send("latitude or longitude is BELOW accepted range:\nlatitude min: 90 \n longitude min: 180")
+    } else if (isNaN(latitude) || isNaN(longitude)){
+        res.send("latitude or longitude is not a number \n input must be number value")
+    } else{
+        getImageURL(latitude, longitude)
+    }
 
-    // //begin calling in image from LRU cache
-    // //I would like a json object
-
-
-    //Call GetImageURL(float, float) function
-    GetImageURL()
+   
 })
 
 // Static directory
@@ -93,5 +83,7 @@ app.use('/', express.static("public"));
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
   
+
+ 
